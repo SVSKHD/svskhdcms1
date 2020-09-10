32e1598d774348c39eb35acb015ba1555b6afd9e
SVSKHD <8svskhd@gmail.com>
2020-09-10 09:27:02 +0530

    images

--- api/subscribers/controllers/subscribers.js
+++ api/subscribers/controllers/subscribers.js
@@ -1,8 +1,14 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.subscribers.create(data, { files });
+    } else {
+      entity = await strapi.services.subscribers.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.subscribers });
+  },
+};
